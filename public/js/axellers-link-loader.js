5c5f5b9924497a1e3ceb80ab82ed738c24404ec9
Oleg256 <oleg@axellers.io>
2021-12-09 10:32:53 +0200

    update load link

--- public/js/axellers-link-loader.js
+++ public/js/axellers-link-loader.js
@@ -7,7 +7,7 @@ const AxellersLoader = {
     var scriptTag = document.createElement("script");
     scriptTag.async = true;
     scriptTag.type = "text/javascript";
-    scriptTag.src = "https://cdn.jsdelivr.net/gh/axellers/react-axellers-link@bbbc66bffc243ef680fd110840e9e6e15b0b73c5/public/js/axellers-link.js";
+    scriptTag.src = "https://cdn.jsdelivr.net/gh/axellers/react-axellers-link@714e3864669a8b3c4b0f692f30d0fc1aa823a57f/public/js/axellers-link.js";
     scriptTag.onload = callback;
 
     var otherScripts = document.getElementsByTagName("script");
